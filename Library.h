f11afc8e3680f37ca8363a4758fb0f523b5ff276
JayJ20000 <111758412+JayJ20000@users.noreply.github.com>
2025-04-04 23:27:13 -0500

    Updated Library.h

--- Library.h
+++ Library.h
@@ -2,24 +2,26 @@
 #define LIBRARY_H
 
 #include "game.h"
-#include <string>
 #include <list>
+#include <string>
 
 using namespace std;
 
 class Library {
- private:
-  list<Game> games;
+private:
+    list<Game> games;
 
- public:
-  Library();
-  ~Library();
+public:
+    Library();
+    ~Library();
 
-  void insert_sorted(string title, string publisher, string genre, float hoursPlayed, float price, int year);
-  void delete_game(string title, int year);
-  void find_game(string title);
-  void find_genre(string genre);
-  void print();
+    void insert_sorted(string title, string publisher, string genre, float hourPlayed, float price, int year);
+    void delete_game(string title, int year);
+    void find_game(string title);
+    void find_genre(string genre);
+    void load_from(const string& filename);
+    void save_to(const string& filename);
+    void print();
 };
 
-#endif // LIBRARY_H library.h
+#endif // LIBRARY_H
