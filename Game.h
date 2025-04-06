1e6150208e64d21f867a65ce4cd7162f588e23ae
JayJ20000 <111758412+JayJ20000@users.noreply.github.com>
2025-04-05 20:42:34 -0500

    Updated Game.h

--- Game.h
+++ Game.h
@@ -1,3 +1,14 @@
+/**
+ * @file game.h
+ * @brief Defines the Game struct
+ * 
+ * Each Game holds  title, publisher, genre,hours played, price, and release year.
+ * 
+ * @author 
+ * Jalen Jones, Mason E
+ * @date 4/3/2025
+ */
+
 #ifndef GAME_H
 #define GAME_H
 
@@ -5,14 +16,17 @@
 
 using namespace std;
 
+/**
+ * @struct Game
+ * @brief Represents a single game entry with properties.
+ */
 struct Game {
-  string title;
-  string publisher;
-  string genre;
-  float hoursPlayed;
-  float price;
-  int year;
-  Game* next;
+    string title;        // Title of the game
+    string publisher;    // Publisher of the game
+    string genre;        // Genre/category of the game
+    float hoursPlayed;   // Total hours the game has been played
+    float price;         // Price of the game in USD
+    int year;            // Year the game was released
 };
 
-#endif // GAME_H here is the game.h
+#endif // GAME_H
